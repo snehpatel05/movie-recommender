70c0d0b695bfa18b596359f09eebfb049f05e9bf
snehpatel05 <itssneh07@gmail.com>
2026-07-09 17:49:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,22 +27,15 @@ movies=pickle.load(open("movies.pkl","rb"))
 similarity=pickle.load(open("similarity.pkl","rb"))
 movies_li=movies['title'].values
 selected_movie_name=st.selectbox("Enter a movie",movies_li)
+
 if st.button("Recommend"):
-    names,posters=recommend(selected_movie_name)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(names[0])
-        st.image(posters[0])
-    with col2:
-        st.text(names[1])
-        st.image(posters[1])
-    with col3:
-        st.text(names[2])
-        st.image(posters[2])
-    with col4:
-        st.text(names[3])
-        st.image(posters[3])
-    with col5:
-        st.text(names[4])
-        st.image(posters[4])
+    names, posters = recommend(selected_movie_name)
     
+    # Creates 5 columns in one row
+    columns = st.columns(5)
+    
+    # Loops through all 5 columns dynamically to display the text and poster in one line
+    for index, col in enumerate(columns):
+        with col:
+            st.text(names[index])
+            st.image(posters[index])
